c08a20230b4552ae836f0fd14a24ab6b6bb9e679
akashstwt <akashpatel115347@gmail.com>
2025-05-07 21:38:06 +0530

    first commit

---
+++ components/Home/AboutMe/AboutMe.tsx
@@ -0,0 +1,120 @@
+import React from "react";
+import Img from "../../../components/smallComp/image/Img";
+import ArrowIcon from "../../../components/Icons/ArrowIcon";
+export default function AboutMe(props) {
+
+  const technologies = [
+    ["Nextjs/React", "TypeScript/JavaScript", "Tailwind CSS", "Flutter/Dart", "AWS/Google Cloud"],
+    ["Node.js", "Java", "Firebase", "Supabase", "RESTful APIs"],
+  ];
+
+  return (
+    <div id="aboutSection" data-aos="fade-up" className="snap-start flex flex-col  items-center py-20 bg-AAprimary">
+      {/* // ? 0.1 About Me */}
+      <div
+        className="flex flex-col space-y-8 px-4 sm:px-0 w-full sm:w-[500px] 
+        md:w-[700px] lg:w-[900px] "
+      >
+        <div className="flex flex-row items-center ">
+          <div className="flex flex-row  items-center mr-4">
+            <ArrowIcon className={"flex-none h-4 md:h-6 w-4 md:w-5 translate-y-[0.5px] text-AAsecondary"} />
+            <span className="text-AAsecondary font-Header text-sm  sm:text-xl"> 01.</span>
+            <span className="flex-none text-gray-200 opacity-85 font-bold tracking-wider text-lg sm:text-2xl pl-4">
+              About Me
+            </span>
+          </div>
+          <div className="bg-gray-400 h-[0.2px] w-full sm:w-72 ml-4"></div>
+        </div>
+        {/* // ? Paragraphs */}
+
+        <div className="w-full flex flex-col md:flex-row space-y-8 md:space-y-0  md:space-x-8 sm:space-x-2 ">
+          <div className="w-full md:w-7/12 space-y-4 sm:text-base text-sm ">
+
+          <div className="font-Header text-justify">
+            <span className="text-gray-400">
+              Hello! My name is Akash, and I&apos;m a passionate <span className="text-AAsecondary">Frontend Developer</span> 
+              and <span className="text-AAsecondary">Flutter Developer</span> with strong problem-solving skills.
+              My journey into programming began when I got inspired by the energy and creativity I saw at 
+              <span className="text-AAsecondary">hackathons</span>—watching people build and explore new 
+              tech sparked my curiosity and love for development.Since then, I&apos;ve been focused on building sleek, interactive, 
+              and high-performance digital experiences using modern frontend technologies and Flutter for mobile platforms.
+            </span>
+          </div>
+
+          <div className="font-Header text-justify">
+            <span className="text-gray-400">
+              I also have a solid foundation in <span className="text-AAsecondary">backend development</span>, which allows me to build 
+              complete solutions and collaborate effectively with cross-functional teams. I&apos;ve worked with companies like 
+              <span className="text-AAsecondary">Permissionless</span> and contributed to impactful projects across various domains, 
+              including international and transportation sectors. Currently, I&apos;m actively <span className="text-AAsecondary">exploring Web3 </span> 
+              and decentralized technologies to stay ahead in the ever-evolving tech space.
+            </span>
+          </div>
+
+            <div className="font-Header tracking-wide text-justify ">
+              <span className="text-gray-400  ">
+                I&apos;m always interested to keep learning and improve my skills to make a positive impact in the tech
+                world. Here are some of the technologies I&apos;ve been working with :{" "}
+              </span>
+            </div>
+            <div className="font-Header tracking-wide flex flex-row space-x-16  justify-center lg:justify-start">
+              <div className="flex flex-row space-x-2 items-center">
+                <div className="flex flex-col space-y-4 sm:text-base text-sm">
+                  {technologies[0].map((tech, index) => {
+                    return (
+                      <div key={index} className="flex flex-row items-center space-x-2">
+                        <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
+                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-row space-x-2 items-center">
+                <div className="flex flex-col space-y-4 sm:text-base text-sm">
+                  {technologies[1].map((tech, index) => {
+                    return (
+                      <div key={index} className="flex flex-row items-center space-x-2">
+                        <ArrowIcon className={"h-3 w-3 text-AAsecondary"} />
+                        <span className="text-gray-400 sm:text-sm text-xs">{tech}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* // ? Image in Desktop and Tablet */}
+          <div className="group relative lg:w-96 lg:h-96 md:w-72 md:h-72 md:block hidden ">
+            <div
+              className="group-hover:translate-x-3 group-hover:translate-y-3
+               duration-300 absolute w-5/6 h-5/6 border-2 border-AAsecondary translate-x-5 
+               translate-y-5 rounded"
+            ></div>
+
+            <div className="absolute w-5/6 h-5/6  rounded overflow-hidden">
+              <div className="absolute w-full h-full group-hover:opacity-0 bg-AAsecondary opacity-10 duration-300 rounded overflow-hidden"></div>
+              <Img
+                src={"/img/Portfolio-portrait-3.jpg"}
+                className={"object-contain rounded-lg"}
+                alt="My Image Not Found"
+              />
+            </div>
+          </div>
+          {/* // ?Image in Mobile */}
+          <div className=" relative w-full h-48 md:hidden  flex justify-center items-center">
+            <div className="absolute w-48 h-full  rounded  translate-x-5 translate-y-5 border-2 border-AAsecondary"></div>
+            <div className="absolute w-48 h-full rounded overflow-hidden">
+              <Img
+                src={"/img/Portfolio-portrait-3.jpg"}
+                className={"object-contain rounded-lg"}
+                alt="My Image Not Found"
+              />
+            </div>
+            <div className="absolute w-48 h-full  bg-AAsecondary opacity-10 md:opacity-60  rounded overflow-hidden"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ components/Home/MyName/MyName.tsx
@@ -0,0 +1,101 @@
+import React from "react";
+import { motion } from "../../../node_modules/framer-motion/dist/framer-motion";
+import Link from "next/link";
+import { useRouter } from "next/router";
+export default function MyName(props: { finishedLoading: boolean }) {
+  const router = useRouter();
+  return (
+    <div
+      className="h-full flex flex-col justify-center
+      px-8 2xl:px-72 xl:px-56 lg:px-32  md:px-28 sm:px-8 py-32 sm:py-52  "
+    >
+      <motion.span
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.4, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.4, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-AAsecondary font-mono"
+      >
+        Hi, my name is
+      </motion.span>
+      <motion.h1
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.5, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.5, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-300 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4"
+      >
+        Akash Patel.
+      </motion.h1>
+      <motion.h2
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.6, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.6, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-400 font-bold text-3xl lg:text-7xl sm:text-5xl md:text-6xl mt-4"
+      >
+        I turn ideas into reality.
+      </motion.h2>
+
+      {/* <motion.h3
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-400 font-Header text-sm md:text-lg sm:text-md mt-10 tracking-wider"
+      >
+        I&apos;m a <span className="text-AAsecondary">software engineer</span>, with strong problem-solving skills. I
+        specialize in building great digital experiences, <br className="2xl:block hidden" />
+        both in <span className="text-AAsecondary">front-end</span> and{" "}
+        <span className="text-AAsecondary">back-end</span> development. I work with different technologies, including
+        <span className="text-AAsecondary"> web3</span> and regular <br className="2xl:block hidden" /> web
+        applications. I enjoy working on projects that bring new ideas and high-quality results.
+      </motion.h3> */}
+      <motion.h3
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.7, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="text-gray-400 font-Header text-sm md:text-lg sm:text-md mt-10 tracking-wider "
+      >
+        I&apos;m a <span className="text-AAsecondary">Frontend Developer</span> passionate about building sleek and high-performance digital experiences.
+        <br className="3xl:block hidden" /> I specialize in crafting responsive and user-friendly web interfaces using modern technologies,
+        with a strong focus on <span className="text-AAsecondary">UI/UX</span> and performance.
+        <br className="3xl:block hidden" /> Alongside web development, I&apos;m also an experienced <span className="text-AAsecondary">Flutter Developer</span>,
+        building cross-platform mobile apps that are fast, interactive, and visually engaging.
+        <br className="3xl:block hidden" /> With a basic understanding of <span className="text-AAsecondary">backend development</span>, 
+        I effectively bridge the gap between design and functionality.
+        <br className="3xl:block hidden" /> I thrive on solving complex challenges and collaborating with teams to turn ideas into
+        <span className="text-AAsecondary"> real-world solutions</span>.
+        <br className="3xl:block hidden" /> I&apos;m excited to contribute to impactful projects that drive innovation and push the boundaries of what technology can do.
+
+        technology.
+      </motion.h3>
+      <motion.div
+        initial={{ y: 10, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{
+          opacity: { delay: props.finishedLoading ? 0 : 10.8, duration: props.finishedLoading ? 0 : 0.2 },
+          y: { delay: props.finishedLoading ? 0 : 10.8, duration: props.finishedLoading ? 0 : 0.2 },
+        }}
+        className="mt-12"
+      >
+        <a href={"/resume.pdf"} target={"_blank"} rel="noreferrer">
+          <button className="bg-AAprimary text-AAsecondary border rounded px-4 sm:px-8 py-3 sm:py-4 border-AAsecondary">
+            Check out my resume!
+          </button>
+        </a>
+      </motion.div>
+    </div>
+  );
+}
